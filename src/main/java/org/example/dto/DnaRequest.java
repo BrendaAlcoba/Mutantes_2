0112ccf483f1daa40d178b3464fd8db29b69b961
BrendaAlcoba <106177189+BrendaAlcoba@users.noreply.github.com>
2025-11-26 16:18:27 -0300

    commit_DNAREQUEST

--- src/main/java/org/example/dto/DnaRequest.java
+++ src/main/java/org/example/dto/DnaRequest.java
@@ -3,13 +3,11 @@ package org.example.dto;
 import lombok.Data;
 import jakarta.validation.constraints.NotNull;
 import org.example.validation.ValidDnaSequence;
-import jakarta.validation.constraints.NotEmpty;
 
 @Data
 public class DnaRequest {
 
     @NotNull(message = "El array de ADN no puede ser nulo.")
-    @NotEmpty(message = "El array de ADN no puede estar vacío.")
     @ValidDnaSequence
     private String[] dna;
 }
